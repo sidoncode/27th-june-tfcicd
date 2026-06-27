ce984ac694d5b7f777d0f4742e99278e6ab4adef
Siddharth Sharma <siddharth54330@gmail.com>
2026-06-27 21:06:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-simple-bucket-12345"   # must be globally unique — change this!
+  bucket = "my-sidpwskills-bucket-007"   # must be globally unique — change this!
 }
